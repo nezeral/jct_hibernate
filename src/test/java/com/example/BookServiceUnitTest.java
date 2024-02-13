56811b5d8f9a0f10fa23d1c7e7f32253c44b1590
acaytan <antoine@dataroots.io>
2024-02-13 10:55:23 +0100

    solving dependencies in POM by adding parents and right plugin

--- src/test/java/com/example/BookServiceUnitTest.java
+++ src/test/java/com/example/BookServiceUnitTest.java
@@ -3,17 +3,16 @@ package com.example;
 import com.example.models.Book;
 import com.example.services.BookService;
 
-import org.junit.Assert;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class BookServiceUnitTest {
 
@@ -23,6 +22,6 @@ public class BookServiceUnitTest {
     @Test
     public void whenApplicationStarts_thenHibernateCreatesInitialRecords() {
         List<Book> books = bookService.list();
-        Assert.assertEquals(books.size(), 3);
+        assertEquals(books.size(), 3);
     }
 }
